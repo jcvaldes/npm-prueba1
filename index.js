d5e1e20c62e7477f47c4230d8e96b53b8f6455a1
Juan Carlos Valdes <idevkingos@gmail.com>
2021-01-20 07:39:29 -0300

    Primera version del modulo de npm prueba de calculadora

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  /**
+   * Suma de dos numeros
+   * @example
+   * n1 = 2, n2 = 2 => resultado 4
+   * @param {*} n1 Numero uno de la suma
+   * @param {*} n2 Numero dos de la suma
+   */
+  suma: function (n1, n2) {
+    return n1 + n2
+  },
+  /**
+   * Resta de dos numeros
+   * @example
+   * n1 = 2, n2 = 2 => resultado 0
+   * @param {*} n1 Numero uno de la resta
+   * @param {*} n2 Numero dos de la resta
+   */
+  resta: function (n1, n2) {
+    return n1 - n2
+  },
+  /**
+   * Multiplicación de dos numeros
+   * @example
+   * n1 = 2, n2 = 2 => resultado 4
+   * @param {*} n1 Numero uno de la multiplicación
+   * @param {*} n2 Numero dos de la multiplicación
+   */
+  multiplicacion: function (n1, n2) {
+    return n1 * n2
+  },
+  /**
+   * División de dos numeros
+   * @example
+   * n1 = 2, n2 = 2 => resultado 1
+   * @param {*} n1 Numero uno de la división
+   * @param {*} n2 Numero dos de la división
+   */
+  division: function (n1, n2) {
+    return n1 / n2
+  }
+}
