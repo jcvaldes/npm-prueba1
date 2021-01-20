642ac1ac7e5243976927e3303a7d968314e5aa3b
Juan Carlos Valdes <idevkingos@gmail.com>
2021-01-20 08:38:33 -0300

    Validacion de numeros aplicada

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
    * @param {*} n2 Numero dos de la suma
    */
   suma: function (n1, n2) {
-    return n1 + n2
+    return this.esNumero(n1,n2) ?  n1 + n2 : this.mensajeError()
   },
   /**
    * Resta de dos numeros
@@ -17,7 +17,7 @@ module.exports = {
    * @param {*} n2 Numero dos de la resta
    */
   resta: function (n1, n2) {
-    return n1 - n2
+    return this.esNumero(n1,n2) ?  n1 - n2 : this.mensajeError()
   },
   /**
    * Multiplicación de dos numeros
@@ -27,7 +27,7 @@ module.exports = {
    * @param {*} n2 Numero dos de la multiplicación
    */
   multiplicacion: function (n1, n2) {
-    return n1 * n2
+    return this.esNumero(n1,n2) ?  n1 * n2 : this.mensajeError()
   },
   /**
    * División de dos numeros
@@ -37,6 +37,25 @@ module.exports = {
    * @param {*} n2 Numero dos de la división
    */
   division: function (n1, n2) {
-    return n1 / n2
+    return this.esNumero(n1,n2) ?  n1 / n2 : this.mensajeError()
+  },
+  /**
+   * Mensaje de error cuando no tengamos valores numericos
+   */
+  mensajeError: function() {
+    console.log('Alguno de los parametros ingresados no es númerico')
+  },
+  /**
+   * Comprueba que son numeros
+   * * @example
+   * esNumero(n1, n2) => resultado: true|false
+   * @param {*} n1 Numero uno
+   * @param {*} n2 Numero dos
+   */
+  esNumero: function(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+      return false
+    }
+    return true
   }
 }
